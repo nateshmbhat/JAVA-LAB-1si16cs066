4e891d4474a763828ec897f208267748ef902221
nateshmbhat <nateshmbhat1@gmail.com>
2018-08-29 16:12:48 +0530

    added stuff

---
+++ JavaLab/Java Lab/labset_5/Main.java
@@ -0,0 +1,7 @@
+import balance.* ;
+
+public class Main{
+	public static void main(String[] args){
+		
+	}
+}
---
+++ JavaLab/Java Lab/labset_5/balance/Balance.java
@@ -0,0 +1,11 @@
+package balance ;
+
+import java.io.* ; 
+import java.util.* ; 
+
+
+public class Balance{
+	public static void println(String s){
+		System.out.println(s) ;
+	}
+}
