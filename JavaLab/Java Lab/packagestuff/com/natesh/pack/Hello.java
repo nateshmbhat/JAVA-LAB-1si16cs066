82c8a1e3b94f31090b3bc35b7a6e08d9c903c3c0
nateshmbhat <nateshmbhat1@gmail.com>
2018-09-05 07:12:13 +0530

    update

---
+++ JavaLab/Java Lab/packagestuff/com/natesh/pack/Hello.java
@@ -0,0 +1,10 @@
+
+package com.natesh.pack ; 
+import java.lang.* ; 
+
+public class Hello{
+    public static void main(String[] args) {
+        System.out.println("useme");
+        lang.System.out.println("im printing") ; 
+    }
+}
---
+++ JavaLab/Java Lab/packagestuff/com/natesh/pack3/Main.java
@@ -0,0 +1,15 @@
+
+package com.natesh.pack3 ; 
+
+import com.natesh.pack.*;
+import com.natesh.nextpack.* ; 
+
+
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("Main method called");
+        
+        Hello.main(args) ;
+        code1.main(args) ; 
+    }
+}
